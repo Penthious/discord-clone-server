990902f8acc090f751da641d066057323817d034
Penthious <tleffew1994@gmail.com>
2020-11-20 18:59:54 -0700

    added in categories

--- migrations/20201027180933_create_categories_table.go
+++ migrations/20201027180933_create_categories_table.go
@@ -2,6 +2,7 @@ package migration
 
 import (
 	"database/sql"
+
 	"github.com/pressly/goose"
 )
 
@@ -11,10 +12,32 @@ func init() {
 
 func Up20201027180933(tx *sql.Tx) error {
 	// This code is executed when the migration is applied.
+	_, err := tx.Exec(`
+CREATE TABLE categories (
+  id        int(11)      NOT NULL AUTO_INCREMENT,
+  name      varchar(100) NOT NULL,
+  server_id int(11)      NOT NULL,
+  
+  PRIMARY KEY (id, server_id),
+
+  FOREIGN KEY (server_id) REFERENCES servers (id)
+)
+ENGINE = 'InnoDB'
+DEFAULT CHARSET = 'utf8mb4'
+COLLATE = 'utf8mb4_unicode_ci'
+`)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func Down20201027180933(tx *sql.Tx) error {
 	// This code is executed when the migration is rolled back.
+	_, err := tx.Exec(`DROP TABLE categories`)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- models/server.go
+++ models/server.go
@@ -6,9 +6,10 @@ import (
 
 type Server struct {
 	gorm.Model
-	Name    string `json:"name"`
-	Private bool   `json:"private"`
-	User_ID uint
-	Users   []*User `gorm:"many2many:server_users"`
-	Roles   []Role
+	Name       string `json:"name"`
+	Private    bool   `json:"private"`
+	User_ID    uint
+	Users      []*User `gorm:"many2many:server_users"`
+	Roles      []Role
+	Categories []Category
 }
